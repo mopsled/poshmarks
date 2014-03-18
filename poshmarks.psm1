a8c48928b6c6fbe047e67126cc6676a2def36cb8
Alec Geatches <throwpi@me.com>
2014-03-17 18:01:45 -0700

    Added usage to commands with parameters

--- poshmarks.psm1
+++ poshmarks.psm1
@@ -2,24 +2,44 @@ $marks = @{}
 $scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 $marksPath = "$scriptDir\poshmarks.csv"
 
-if(test-path $marksPath){
+if (test-path $marksPath) {
     import-csv $marksPath | %{$marks[$_.key]=$_.value}
 }
 
-function j($number) {
-    cd $marks["$number"]
+function j($name) {
+    if (!$name) {
+        return usage
+    }
+
+    cd $marks[$name]
 }
 
 function jl {
     $marks
 }
 
-function js($number) {
-    $marks["$number"] = (pwd).path
+function js($name) {
+    if (!$name) {
+        return usage
+    }
+
+    $marks[$name] = (pwd).path
     $marks.getenumerator() | export-csv $marksPath -notype
 }
 
-function jd($number) {
-    $marks.remove("$number")
+function jd($name) {
+    if (!$name) {
+        return usage
+    }
+
+    $marks.remove($name)
     $marks.getenumerator() | export-csv $marksPath -notype
+}
+
+function usage() {
+    Write-Host
+    Write-Host "  js <name> - Saves the current directory as `"name`""
+    Write-Host "  j  <name> - Go to directory with jumpmark `"name`""
+    Write-Host "  jd <name> - Delete jumpmark `"name`""
+    Write-Host "  jl        - List jumpmarks"
 }
